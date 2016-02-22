376f1632c427c8fc562289a23d2513f6e579c207
Neal Richardson <neal.p.richardson@gmail.com>
2016-02-22 08:36:54 -0800

    Ensure that all public functions are documented and exported

--- R/log.R
+++ R/log.R
@@ -10,6 +10,21 @@ logMessage <- function (...) {
     }
 }
 
+##' Stop, log, and no call
+##'
+##' Wrapper around \code{stop} that logs the error message and then stops
+##' with call.=FALSE by default.
+##' @param ... arguments passed to \code{stop}
+##' @param call. logical: print the call? Default is \code{FALSE}, unlike
+##' \code{stop}
+##' @return Nothing. Raises an error.
+##' @export
+halt <- function (..., call.=FALSE) {
+    msg <- gsub("\n", " ", ..1)
+    logMessage("ERROR", msg)
+    stop(..., call.=call.)
+}
+
 ##' Enable logging
 ##'
 ##' @param filename character: a filename/path where the log can be written out.
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -5,6 +5,10 @@ options(warn=1)
 cacheOn()
 
 source("mocks.R")
-with(globalenv(), source("mocks.R"))
 
 public <- function (...) with(globalenv(), ...)
+
+public({
+    source("mocks.R")
+    cacheKeys <- function () ls(envir=httpcache:::cache)
+})
